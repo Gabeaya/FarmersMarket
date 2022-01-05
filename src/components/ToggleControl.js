adc30e0c72652abbd09320956e958fdba651a29d
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-04 20:49:59 -0800

    Create class component for handling the toggle of produce and market schedule.

--- src/components/ToggleControl.js
+++ src/components/ToggleControl.js
@@ -10,10 +10,27 @@ class ToggleControl extends React.Component {
       produceVisibleOnPage: false
     };
   }
+  
+  handleClick = () => {
+    this.setState(prevState => ({
+      produceVisibleOnPage: !prevState.produceVisibleOnPage
+    }));
+  }
 
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.produceVisibleOnPage) {
+      currentlyVisibleState = <AvailableProduce />
+      buttonText = "Return to Market Schedule";
+    } else {
+      currentlyVisibleState = <MarketSchedule />
+      buttonText = "Check Out Produce"
+    }
     return (
       <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
