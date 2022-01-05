cae9cae43582edaa7b948f8845238ebfae09057d
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-04 18:49:06 -0800

    Add an image to the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
+import fruitImage from "./../img/fruit.jpeg";
 
 function Header(){
   return (
+    <React.Fragment>
       <h1>Avery's Organics</h1>
+      <img src={fruitImage} alt="A photo of an assortment of fruit/veggies"/>
+    </React.Fragment>
+      
   );
 }
 
